967749ae55a5e3c9991d9afa7c6745409e13d0c7
HongboQiao <46460674+HongboQiao@users.noreply.github.com>
2019-03-12 15:16:29 -0700

    Update SMS_BandGap.py

--- Organic-Semi/Documents/SMS_BandGap.py
+++ Organic-Semi/Documents/SMS_BandGap.py
@@ -12,23 +12,6 @@ from multiprocessing import freeze_support
 from rdkit import Chem
 from mordred import Calculator, descriptors
 
-sms = input("Please in put a valid SMILES string: ")
-
-import numpy as np
-import pandas as pd
-import pickle
-
-from sklearn import linear_model
-from pandas.plotting import scatter_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from multiprocessing import freeze_support
-from rdkit import Chem
-from mordred import Calculator, descriptors
-
-# sms = input("Please in put a valid SMILES string: ")
 
 def sms_bandgap(sms):
     """Function from sms to predict Bandgap"""
