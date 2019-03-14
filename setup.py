6059b25e6592db5f4777e3303e5d84099aa22f57
HongboQiao <46460674+HongboQiao@users.noreply.github.com>
2019-03-14 15:28:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+import sys
+sys.path.insert(0, "Organic-Semi/Documents")
+import SMS_BandGap
 
+sms = input("Please Input a SMILES String: ")
+
+BG = SMS_BandGap.sms_bandgap(sms)
+
+print("The Predictied Band Gap is: ", BG)
+EXIT01 = input('Please Press Enter to Exit')
