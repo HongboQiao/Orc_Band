5f7772b46635f67398f769cef45286f0edcadf04
HongboQiao <46460674+HongboQiao@users.noreply.github.com>
2019-03-19 13:00:50 -0700

    Update rfr_optimization.py

--- orcband/ml_model/code/rfr_optimization.py
+++ orcband/ml_model/code/rfr_optimization.py
@@ -13,22 +13,26 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.datasets import load_digits
 from sklearn.ensemble import RandomForestClassifier
 
+
 def search_hyperparameter():
     data = pd.read_csv('../../Documents/Data/DescriptorsDataset.csv')
-    X = data[['AXp-0d', 'AXp-1d', 'AXp-2d', 'ETA_eta_L', 'ETA_epsilon_3']].values
+    X = data[['AXp-0d', 'AXp-1d', 'AXp-2d', 'ETA_eta_L',
+              'ETA_epsilon_3']].values
     y = data[['e_gap_alpha']].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y.ravel(), test_size = .25, random_state=1234)
+    X_train, X_test, y_train, y_test = train_test_split(X, y.ravel(),
+                                                        test_size=.25,
+                                                        random_state=1234)
     param_dist = {"max_depth": [3, None],
-              "min_samples_split": sp_randint(2, 30),
-              "bootstrap": [True, False]}
+                  "min_samples_split": sp_randint(2, 30),
+                  "bootstrap": [True, False]}
     n_iter_search = 20
-    rfr = RandomForestRegressor(n_estimators = 300, random_state = 123)
+    rfr = RandomForestRegressor(n_estimators=300, random_state=123)
     random_search = RandomizedSearchCV(rfr, param_distributions=param_dist,
-                                   n_iter=n_iter_search, cv=5)
+                                       n_iter=n_iter_search, cv=5)
     start = time()
     random_search.fit(X_train, y_train)
     print("RandomizedSearchCV took %.2f seconds for %d candidates"
-      " parameter settings." % ((time() - start), n_iter_search))
+          " parameter settings." % ((time() - start), n_iter_search))
     results = random_search.cv_results_
     for i in range(1, 6):
         candidates = np.flatnonzero(results['rank_test_score'] == i)
