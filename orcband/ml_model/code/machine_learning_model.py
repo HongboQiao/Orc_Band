89413ef5ddb826cf0b87e5e3d8f34f1a5c636481
Yuhuan Meng <44456626+yuhuan94@users.noreply.github.com>
2019-03-19 12:45:20 -0700

    Update and rename Machine_Learning_Model.py to machine_learning_model.py

--- orcband/ml_model/code/machine_learning_model.py
+++ orcband/ml_model/code/machine_learning_model.py
@@ -15,7 +15,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
 from keras import regularizers
-from Evaluation import r2_score
+from evaluation import r2_score
 
 
 def Multiple_Linear_Model(data):
