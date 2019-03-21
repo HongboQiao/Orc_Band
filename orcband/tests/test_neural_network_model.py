888b51c0223459ef104cce3496b5a583acd51696
Yuhuan Meng <44456626+yuhuan94@users.noreply.github.com>
2019-03-21 11:42:22 -0700

    Update test_neural_network_model.py

--- orcband/tests/test_neural_network_model.py
+++ orcband/tests/test_neural_network_model.py
@@ -3,7 +3,7 @@ sys.path.append("../ml_model/code")
 import neural_network_model
 import pandas as pd
 def test_multiple_linaer_model():
-    data = pd.read_csv('../PreML/DescriptorsDataset.csv')
+    data = pd.read_csv('../../documentation/data/DescriptorsDataset.csv')
     score = neural_network_model.neural_network_model(data)
     assert score < 0.6, "should be 0.56"
     return
