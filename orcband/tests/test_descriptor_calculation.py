031e1fbea4df6b7060223dc2375de3144b18259b
yuhuan94 <yuhuan@uw.edu>
2019-03-21 11:05:44 -0700

    change path

--- orcband/tests/test_descriptor_calculation.py
+++ orcband/tests/test_descriptor_calculation.py
@@ -1,10 +1,11 @@
 import sys
-sys.path.append("../PreML")
+sys.path.append("../dataming/code")
 import sms_dcp_csv
 import numpy as np
 import pandas as pd
 
 def test_sms_dcp_csv():
+    """This function is used to test sms_dcp_csv function"""
     data = pd.read_csv('C:/Users/meng1/Desktop/data_science/HCEPDB_moldata/HCEPDB_moldata.csv')
     data_sample = data.sample(frac=0.001)
     result = sms_dcp_csv.sms_dcp(data_sample)
