0b1587b1e2ec002e4583b843d9c224e3232ab535
Yuhuan Meng <44456626+yuhuan94@users.noreply.github.com>
2019-03-21 16:49:07 -0700

    Update test_evaluate.py

--- orcband/tests/test_evaluate.py
+++ orcband/tests/test_evaluate.py
@@ -7,21 +7,21 @@ df1 = np.array([[2],[4],[6]])
 df2 = np.array([[3],[5],[7]])
 def test_mean_square_error():
     a = evaluation.mean_square_error(df1,df2)
-    assert a == 1, "should be 1"
+    assert a == 1, "Error: The function of calculating MSE is wrong!"
     return
 
 def test_mean_absolute_error():
     b = evaluation.mean_absolute_error(df1,df2)
-    assert b == 1, "should be 1"
+    assert b == 1, "Error: The function of calculating MAE is wrong!"
     return
 
 def test_mean_absolute_percentage_error():
     c = evaluation.mean_absolute_percentage_error(df1,df2)
-    assert c < 0.5, "should be less than 0.5"
+    assert round(c,2) == 0.31, "Error: The function of calculating MAPE is wrong!"
     return
 
 def test_r2_score():
     d = evaluation.r2_score(df1,df2)
-    assert d < 1, "should be less than 1"
+    assert d == 0.625, "Error: The function of calculating R2 score is wrong!"
     return
     
