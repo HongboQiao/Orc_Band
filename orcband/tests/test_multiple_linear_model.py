0c5ede8760f07c4ff93d58128b8cbfcd7a973b76
Yuhuan Meng <44456626+yuhuan94@users.noreply.github.com>
2019-03-21 11:37:32 -0700

    change path

--- orcband/tests/test_multiple_linear_model.py
+++ orcband/tests/test_multiple_linear_model.py
@@ -1,11 +1,11 @@
 #test 
 import sys
-sys.path.append("../ML_model")
-import Multiple_linearReg_model
+sys.path.append("../ml_model/code")
+import multiple_linear_model
 import pandas as pd
 def test_multiple_linaer_model():
-    data = pd.read_csv('../PreML/DescriptorsDataset.csv')
-    score = Multiple_linearReg_model.Multiple_Linear_Model(data)
+    data = pd.read_csv('../../documentation/data/DescriptorsDataset.csv')
+    score = multiple_linear_model.Multiple_Linear_Model(data)
     assert score < 0.6, "should be 0.57"
     return
 
