1d00d094ddc23f9b74da121fe4cdbe5190181897
Yuhuan Meng <44456626+yuhuan94@users.noreply.github.com>
2019-03-21 11:44:05 -0700

    Update test_polynomial_Regression.py

--- orcband/tests/test_polynomial_Regression.py
+++ orcband/tests/test_polynomial_Regression.py
@@ -1,9 +1,9 @@
 import sys
-sys.path.append("../ML_model")
-import Polynomial_Regression_model
+sys.path.append("../ml_model/code")
+import polynomial_regression_model
 import pandas as pd
 def test_polynomial_Reg():
-    data = pd.read_csv('../PreML/DescriptorsDataset.csv')
-    score = Polynomial_Regression_model.Polynomial_Model(data)
+    data = pd.read_csv('../../documentation/data/DescriptorsDataset.csv')
+    score = polynomial_regression_model.Polynomial_Model(data)
     assert score < 0.65, "should be 0.64"
     return
