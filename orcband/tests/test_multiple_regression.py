95bc6d3499cd198e848956e60b38c8faaf6be1de
Yuhuan Meng <44456626+yuhuan94@users.noreply.github.com>
2019-03-21 11:40:08 -0700

    Update test_multiple_regression.py

--- orcband/tests/test_multiple_regression.py
+++ orcband/tests/test_multiple_regression.py
@@ -1,28 +1,28 @@
 import sys
-sys.path.append("../ML_model")
-import MLModels
+sys.path.append("../ml_model/code")
+import machine_learning_model
 import pandas as pd
 
 def test_multiple_linaer_model():
-    data = pd.read_csv('../PreML/DescriptorsDataset.csv')
-    score = MLModels.Multiple_Linear_Model(data)
+    data = pd.read_csv('../../documentation/data/DescriptorsDataset.csv')
+    score = machine_learning_model.Multiple_Linear_Model(data)
     assert score < 0.6, "should be 0.57"
     return
 
 def test_Polynomial_Model():
-    data = pd.read_csv('../PreML/DescriptorsDataset.csv')
-    score1 = MLModels.Polynomial_Model(data)
+    data = pd.read_csv('../documentation/data/DescriptorsDataset.csv')
+    score1 = machine_learning_model.Polynomial_Model(data)
     assert score1 < 0.65, "should be less than 0.65"
     return
 
 def test_Random_Forest_Model():
-    data = pd.read_csv('../PreML/DescriptorsDataset.csv')
-    score2 = MLModels.Random_Forest_Model(data)
+    data = pd.read_csv('../documentation/data/DescriptorsDataset.csv')
+    score2 = machine_learning_model.Random_Forest_Model(data)
     assert score2 > 0.7, "should be larger than 0.7"
     return
 
 def test_Neural_Network_Model():
-    data = pd.read_csv('../PreML/DescriptorsDataset.csv')
-    score3 = MLModels.Neural_Network_Model(data)
+    data = pd.read_csv('../documentation/data/DescriptorsDataset.csv')
+    score3 = machine_learning_model.Neural_Network_Model(data)
     assert score3 < 0.6, "should be about 0.56"
     return
