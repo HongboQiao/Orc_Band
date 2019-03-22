e579e1010ffdb43ed73d4df9066a5d36bc730342
Yuhuan Meng <44456626+yuhuan94@users.noreply.github.com>
2019-03-21 17:25:47 -0700

    Update test_Random_Forest_Regression.py

--- orcband/tests/test_Random_Forest_Regression.py
+++ orcband/tests/test_Random_Forest_Regression.py
@@ -1,9 +1,22 @@
 import sys
 sys.path.append("../ml_model/code")
-import random_forest_regression
+import random_forest_regression_model
 import pandas as pd
+
 def test_Random_Forest_Reg():
+    """This function is to test random forest regression code"""
     data = pd.read_csv('../../documentation/data/DescriptorsDataset.csv')
-    score = random_forest_regression.Random_Forest_Reg(data)
-    assert score > 0.7, "should be 0.7"
+    result = random_forest_regression_model.Random_Forest_Reg(data)
+    xname = plt.gca().get_xlabel()
+    yname = plt.gca().get_ylabel()
+    title = plt.gca().get_title()
+    xscale = plt.gca().get_xlim()
+    yscale = plt.gca().get_ylim()
+    assert round(result,2) == 0.7, "Error: The code for r2 score is wrong!"
+    assert xname == '$<Eg> \\ Actual \\ [eV]$', "The figure x label is wrong!"
+    assert yname == '$<Eg> \\ Predict \\ [eV]$', "The figure y label is wrong!"
+    assert title == '$Random \\ Forest \\ Regression$', "The figure title is wrong!"
+    assert xscale[0] == -0.2 and xmin[1] == 4.2, "The range of x is wrong!"
+    assert yscale[0] == -0.2 and xmin[1] == 4.2, "The range of y is wrong!"
+    
     return
