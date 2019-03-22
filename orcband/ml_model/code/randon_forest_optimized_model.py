aa6e998a8c70cd5d033e7b6b59d27ff7e962ea1c
HongboQiao <46460674+HongboQiao@users.noreply.github.com>
2019-03-22 00:28:46 -0700

    fix typo & pep8 compliant

--- orcband/ml_model/code/randon_forest_optimized_model.py
+++ orcband/ml_model/code/randon_forest_optimized_model.py
@@ -6,9 +6,9 @@ from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-from evaluation import r2_score
-from evaluation import mean_square_error
-from evaluation import mean_absolute_error
+from evaluate import r2_score
+from evaluate import mean_square_error
+from evaluate import mean_absolute_error
 
 
 def Random_Forest_Reg(data):
@@ -30,26 +30,25 @@ def Random_Forest_Reg(data):
     # Visualising the Random Forest Regression results
     # in higher resolution and smoother curve
     font1 = {
-           'weight' : 'normal',
-           'size'   : 10,
+           'weight': 'normal',
+           'size': 10,
            }
-    RFR_score = round(r2_score(y_test, Ytest_Pred),2)
-    MAE = round(mean_absolute_error(y_test, Ytest_Pred),2)
-    MSE = round(mean_square_error(y_test, Ytest_Pred),2)
-    datas = [MSE, MAE, RFR_score]
-    n_rows = len(datas)
-    rows = ['PCE', 'MAE', '$R^2$']
-    cell_text = []
-    for row in range(n_rows):
-        cell_text.append([datas[row]])
-    
-    plt.scatter(y_train, Ytrain_Pred, color='blue',label = 'Train')
-    plt.scatter(y_test, Ytest_Pred, color='red', label = 'Test')
-    plt.plot([0, 4], [0, 4], lw=4, color='black', label = 'Y=X')
-    plt.legend(loc='upper left',prop=font1)
+    RFR_score = round(r2_score(y_test, Ytest_Pred), 2)
+    MAE = round(mean_absolute_error(y_test, Ytest_Pred), 2)
+    MSE = round(mean_square_error(y_test, Ytest_Pred), 2)
+    rows = ['MSE', 'MAE', '$R^2$']
+    cell_text = [['MSE', MSE], ['MAE', MAE], ['$R^2$', RFR_score]]
+
+    plt.figure(figsize=(6, 6))
+    plt.scatter(y_train, Ytrain_Pred, color='blue', label='Train')
+    plt.scatter(y_test, Ytest_Pred, color='red', label='Test')
+    plt.plot([0, 4], [0, 4], lw=4, color='black', label='Y=X')
+    plt.legend(loc='upper left', prop=font1)
     plt.title('$Random \ Forest \ Regression$')
-    plt.xlabel('$<Eg> \ Actual \ [eV]$')
-    plt.ylabel('$<Eg> \ Predict \ [eV]$')
-    plt.table(cellText = cell_text,colWidths = [0.15]*3, rowLabels = rows,
-              loc='lower right',bbox = [0.75, 0.05, 0.2, 0.2])
+    plt.xlabel('$<E_g> \ Actual \ [eV]$')
+    plt.ylabel('$<E_g> \ Predict \ [eV]$')
+    my_table = plt.table(cellText=cell_text, colWidths=[0.15]*2, colLoc='center',
+                         cellLoc='center', loc='lower right',
+                         bbox=[0.75, 0.05, 0.2, 0.2])
+    my_table.set_fontsize(20)
     return RFR_score, MAE, MSE
