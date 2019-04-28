60aa5f963fad984a6b88590ac91b884fbc30f49d
Liang <xuliang1@uw.edu>
2019-04-27 23:02:21 -0700

    revise setup

--- documentation/docs/sms_to_bandgap.py
+++ documentation/docs/sms_to_bandgap.py
@@ -12,29 +12,24 @@ from multiprocessing import freeze_support
 from rdkit import Chem
 from mordred import Calculator, descriptors
 
-
 def sms_bandgap(sms):
     """Function from sms to predict Bandgap"""
-    freeze_support()
-    mols = [Chem.MolFromSmiles(sms)]
-    # Create Calculator
-    calc = Calculator(descriptors)
-    # map method calculate multiple molecules (return generator)
-    # pandas method calculate multiple molecules (return pandas DataFrame)
-    raw_data = calc.pandas(mols)
+    freeze_support()   
+    mols = Chem.MolFromSmiles(sms) #transform smiles string to molecular structure
+    if mols is None:
+        raise TypeError('Invalid Smiles String type')
+    else:
+        m = [Chem.MolFromSmiles(sms)]
+        calc = Calculator(descriptors)
+        raw_data = calc.pandas(m) #calculate descriptors
+        new = {'AXp-0d': raw_data['AXp-0d'].values,
+               'AXp-1d': raw_data['AXp-1d'].values,
+               'AXp-2d': raw_data['AXp-2d'].values,
+               'ETA_eta_L': raw_data['ETA_eta_L'].values,
+               'ETA_epsilon_3': raw_data['ETA_epsilon_3'].values}  # extract the five most useful descriptors data
+        new_data = pd.DataFrame(index=[1], data=new)
+        with open('docs/regressor.pickle', 'rb') as f:
+            regressor2 = pickle.load(f)
+        bandgap = regressor2.predict(new_data)  # calculate bandgap
+        return bandgap
 
-    new = {'AXp-0d': raw_data['AXp-0d'].values,
-           'AXp-1d': raw_data['AXp-1d'].values,
-           'AXp-2d': raw_data['AXp-2d'].values,
-           'ETA_eta_L': raw_data['ETA_eta_L'].values,
-           'ETA_epsilon_3': raw_data['ETA_epsilon_3'].values}
-    # Save the predictors (Pending)
-    new_data = pd.DataFrame(index=[1], data=new)
-
-    with open('docs/regressor.pickle', 'rb') as f:
-        regressor2 = pickle.load(f)
-    # path and model need to rewrite
-
-    bandgap = regressor2.predict(new_data)
-
-    return bandgap
